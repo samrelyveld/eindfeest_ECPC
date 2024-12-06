7c77e031118f21627fa6572282fbe796e8f972be
samrelyveld <samrelyveld@gmail.com>
2024-12-06 16:00:49 +0100

    update

--- labwatching/src/labwatching/general_output.py
+++ labwatching/src/labwatching/general_output.py
@@ -2,15 +2,20 @@ import nidaqmx
 from nidaqmx.constants import AcquisitionType
 
 with nidaqmx.Task() as task:
-    data = []
-    total_samples = 1000
-    task.ao_channels.add_ao_voltage_chan("Dev1/ao0")
-    task.timing.cfg_samp_clk_timing(
-        1000.0, sample_mode=AcquisitionType.FINITE, samps_per_chan=total_samples
-    )
+    task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
+    task.timing.cfg_samp_clk_timing(1000.0, sample_mode=AcquisitionType.CONTINUOUS)
+    task.start()
+    print("Running task. Press Ctrl+C to stop.")
 
-    data = [5.0 * i / total_samples for i in range(total_samples)]
-    number_of_samples_written = task.write(data, auto_start=True)
-    print(f"Generating {number_of_samples_written} voltage samples.")
-    task.wait_until_done()
-    task.stop()
+    try:
+        total_read = 0
+        while True:
+            data = task.read(number_of_samples_per_channel=1000)
+            read = len(data)
+            total_read += read
+            print(f"Acquired data: {read} samples. Total {total_read}.", end="\r")
+    except KeyboardInterrupt:
+        pass
+    finally:
+        task.stop()
+        print(f"\nAcquired {total_read} total samples.")
