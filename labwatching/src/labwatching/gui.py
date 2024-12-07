90f4dccb30b0e0ce20a84fcb83feb2430651a973
samrelyveld <samrelyveld@gmail.com>
2024-12-07 14:48:12 +0100

    optie box erbij

--- labwatching/src/labwatching/gui.py
+++ labwatching/src/labwatching/gui.py
@@ -5,7 +5,7 @@ import numpy as np
 from PySide6 import QtWidgets
 from PySide6.QtCore import Slot
 from PySide6.QtGui import QKeySequence, QShortcut
-# from labwatching.model_test import data_analysis
+#from labwatching.model_test import data_analysis
 
 pg.setConfigOption("background", "w")
 pg.setConfigOption("foreground", "k")
@@ -31,20 +31,38 @@ class TabWidgetApp(QtWidgets.QMainWindow):
         self.tab1 = QtWidgets.QWidget()
         self.tab2 = QtWidgets.QWidget()
         self.tab3 = QtWidgets.QWidget()
+        self.tab4 = QtWidgets.QWidget()
 
-        self.tab_widget.addTab(self.tab1, "Tab 1")
-        self.tab_widget.addTab(self.tab2, "Tab 2")
-        self.tab_widget.addTab(self.tab3, "Tab 3")
+        self.tab_widget.addTab(self.tab4, "overzicht, menu")
+        self.tab_widget.addTab(self.tab1, "grafiek")
+        self.tab_widget.addTab(self.tab2, "Tab opslaan")
+        self.tab_widget.addTab(self.tab3, "Tab 4")
+
 
         self.layout.addWidget(self.tab_widget)
-
+        
+        self.initTab4()
         self.initTab1()
         self.initTab2()
         self.initTab3()
 
+
         self.key_binding()
 
         self.short_cut()
+
+
+    def initTab4(self):
+        layout = QtWidgets.QVBoxLayout(self.tab4)
+        label = QtWidgets.QLabel("Content of Tab 1")
+
+        self.textedit = QtWidgets.QTextEdit()
+        self.textedit.append("Alt 1,2,3,4 is tab bladen")
+        self.textedit.append("ctrl+s = data oplaan")
+        self.textedit.append("alt+c plot weg")
+        
+        layout.addWidget(self.textedit)
+
 
     def initTab1(self):
         layout = QtWidgets.QVBoxLayout(self.tab1)
@@ -57,17 +75,41 @@ class TabWidgetApp(QtWidgets.QMainWindow):
     def initTab2(self):
         layout = QtWidgets.QVBoxLayout(self.tab2)
         label = QtWidgets.QLabel("Content of Tab 2")
+        save_button = QtWidgets.QPushButton("save")
+        
+        layout.addWidget(save_button)
+        save_button.clicked.connect(self.save_data)
         layout.addWidget(label)
 
     def initTab3(self):
-        layout = QtWidgets.QVBoxLayout(self.tab3)
         label = QtWidgets.QLabel("Content of Tab 3")
-
-        self.textedit = QtWidgets.QTextEdit()
-        layout.addWidget(self.textedit)
-
-        # Add the label
+        layout = QtWidgets.QHBoxLayout(self.tab3)
         layout.addWidget(label)
+
+
+        textedit = QtWidgets.QTextEdit("text")
+        textedit.append("kies wat je bij je meting wil")
+
+
+        
+        group_box = QtWidgets.QGroupBox("Group Box")
+        group_layout = QtWidgets.QVBoxLayout(group_box)
+
+        option_1 = QtWidgets.QCheckBox("wel onzekerheid")
+        option_2 = QtWidgets.QCheckBox("iets anders")
+        option_1.setChecked(True)
+
+        
+        group_layout.addWidget(option_1)
+        group_layout.addWidget(option_2)
+
+
+
+
+        layout.addWidget(group_box)
+        layout.addWidget(textedit)
+        group_box.setLayout(group_layout)
+
     def plot(self):
         """plots the sine function
         """
@@ -85,8 +127,12 @@ class TabWidgetApp(QtWidgets.QMainWindow):
     def short_cut(self):
         shortcut_shift_w = QShortcut(QKeySequence("Shift+w"), self)
         shortcut_shift_w.activated.connect(self.add_text_button_clicked)
-        shortcut_clear = QShortcut(QKeySequence("Alt+8"), self)
+
+        shortcut_clear = QShortcut(QKeySequence("Alt+c"), self)
         shortcut_clear.activated.connect(self.plot_remover)
+
+        shortcut_save = QShortcut(QKeySequence("Ctrl+s"), self)
+        shortcut_save.activated.connect(self.save_data)
 
     def plot_remover(self):
         self.plot_widget.clear()
@@ -94,6 +140,16 @@ class TabWidgetApp(QtWidgets.QMainWindow):
     def add_text_button_clicked(self):
         self.textedit.append("You clicked me.")
 
+
+    @Slot()
+    def save_data(self):
+        filename, _ = QtWidgets.QFileDialog.getSaveFileName(filter="CSV files (*.csv)")
+
+        with open(filename, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)    
+            writer.writerow(['Hz', 'Hz_std', 'fWHM', 'amp'])
+            for a, b, c, d, in zip(self.frequentie, self.std, self.FWHM, self.ampl):
+                writer.writerow([a, b, c, d])
 def main():
     app = QtWidgets.QApplication(sys.argv)
     ui = TabWidgetApp()
